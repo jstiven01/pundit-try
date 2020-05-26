ce1e4bf84cc56d3bf46110eb19db04887340b9e7
Johan Tinjaca <johantinjaca@Johans-MacBook-Pro.local>
2020-05-26 17:05:03 -0500

    Adding policy to controller article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @articles = Article.all
+    authorize @articles
   end
 
   # GET /articles/1
@@ -15,6 +16,7 @@ class ArticlesController < ApplicationController
   # GET /articles/new
   def new
     @article = Article.new
+    authorize @article
   end
 
   # GET /articles/1/edit
@@ -25,6 +27,8 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
+    @article.user = current_user
+    authorize @article
 
     respond_to do |format|
       if @article.save
@@ -65,6 +69,7 @@ class ArticlesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
+      authorize @article
     end
 
     # Only allow a list of trusted parameters through.
---
+++ app/policies/article_policy.rb
@@ -0,0 +1,24 @@
+class ArticlePolicy < ApplicationPolicy
+    def index?
+        true
+    end
+
+    def create?
+        user.present? # Registered User
+    end
+
+    def update?
+        return true if user.present? && user == article.user
+    end
+
+    def destroy?
+        return true if user.present? && user == article.user
+    end
+
+    private
+
+    def article
+        record
+    end
+
+end
